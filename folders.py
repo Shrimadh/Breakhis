7146f4b3aedf319febfb7277a1d00870a61733f8
Shrimadh Rao <shrimadh332001@gmail.com>
2021-05-09 12:40:21 +0530

    Commit

--- folders.py
+++ folders.py
@@ -5,43 +5,43 @@ import numpy as np
 import csv
 
 
-csvi = "C:/Users/DELL/Desktop/Folds.csv"
+csvi = "../Data/archive/Folds.csv"
 data = pd.read_csv(csvi)
 
 
 for i,d in enumerate(data["filename"]):
-
+    print(i)
     if(d.find("benign")!=-1):
         if(data["grp"][i]=='train'):
-            img = cv2.imread("C:/Users/DELL/Desktop/archive/BreaKHis_v1/"+d)
-            cv2.imwrite("C:/Users/DELL/Desktop/Cancer/Train/Benign/"+str(i)+".jpg",img)
-            with open('data.csv', 'a', newline='') as csvfile:
+            img = cv2.imread("../Data/archive/BreaKHis_v1/"+d)
+            cv2.imwrite("../Data/Cancer/Train/Benign/"+str(i)+".jpg",img)
+            with open('train.csv', 'a', newline='') as csvfile:
                 fieldnames = ['Image','Benign']
                 writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                writer.writerow({'Image':"C:/Users/DELL/Desktop/Cancer/Train/Benign/"+str(i)+".jpg", 'Benign':1})
+                writer.writerow({'Image':"../Data/Cancer/Train/Benign/"+str(i)+".jpg", 'Benign':1})
             pass
         else:
-            img = cv2.imread("C:/Users/DELL/Desktop/archive/BreaKHis_v1/"+d)
-            cv2.imwrite("C:/Users/DELL/Desktop/Cancer/Test/Benign/"+str(i)+".jpg",img)
+            img = cv2.imread("../Data/archive/BreaKHis_v1/"+d)
+            cv2.imwrite("../Data/Cancer/Test/Benign/"+str(i)+".jpg",img)
             with open('test.csv', 'a', newline='') as csvfile:
                 fieldnames = ['Image','Benign']
                 writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                writer.writerow({'Image':"C:/Users/DELL/Desktop/Cancer/Test/Benign/"+str(i)+".jpg", 'Benign':1})
+                writer.writerow({'Image':"../Data/Cancer/Test/Benign/"+str(i)+".jpg", 'Benign':1})
 
     else:
         if(data["grp"][i]=="train"):
-            img = cv2.imread("C:/Users/DELL/Desktop/archive/BreaKHis_v1/"+d)
-            cv2.imwrite("C:/Users/DELL/Desktop/Cancer/Train/Malignant/"+str(i)+".jpg",img)
-            with open('data.csv', 'a', newline='') as csvfile:
+            img = cv2.imread("../Data/archive/BreaKHis_v1/"+d)
+            cv2.imwrite("../Data/Cancer/Train/Malignant/"+str(i)+".jpg",img)
+            with open('train.csv', 'a', newline='') as csvfile:
                 fieldnames = ['Image','Benign']
                 writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                writer.writerow({'Image':"C:/Users/DELL/Desktop/Cancer/Train/Malignant/"+str(i)+".jpg", 'Benign':0})
+                writer.writerow({'Image':"../Data/Cancer/Train/Malignant/"+str(i)+".jpg", 'Benign':0})
             pass
         else:
-            img = cv2.imread("C:/Users/DELL/Desktop/archive/BreaKHis_v1/"+d)
-            cv2.imwrite("C:/Users/DELL/Desktop/Cancer/Test/Malignant/"+str(i)+".jpg",img)
+            img = cv2.imread("../Data/archive/BreaKHis_v1/"+d)
+            cv2.imwrite("../Data/Cancer/Test/Malignant/"+str(i)+".jpg",img)
             with open('test.csv', 'a', newline='') as csvfile:
                 fieldnames = ['Image','Benign']
                 writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-                writer.writerow({'Image':"C:/Users/DELL/Desktop/Cancer/Test/Malignant/"+str(i)+".jpg", 'Benign':0})
+                writer.writerow({'Image':"../Data/Cancer/Test/Malignant/"+str(i)+".jpg", 'Benign':0})
 
